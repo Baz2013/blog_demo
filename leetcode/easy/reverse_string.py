7719382cf85250f775952135e3465966d68980c4
gucb <chuan.gu6@gmail.com>
2016-08-16 22:44:46 +0800

    resolve str reverse issue

---
+++ leetcode/easy/reverse_string.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        # if type(s) is not str:
+        #     return
+
+        if s is None or s == "":
+            return ''
+        else:
+            pass
+
+        return s[::-1]
+
+
+# test
+solution = Solution()
+print solution.reverseString("hello")
+print solution.reverseString('')
+print solution.reverseString(123)
+
