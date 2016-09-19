6ee41708a3b210a99270f30b06579c415f8a9b03
gucb <chuan.gu6@gmail.com>
2016-09-19 19:54:25 +0800

    modified:   easy/binary_watch.py

--- leetcode/easy/binary_watch.py
+++ leetcode/easy/binary_watch.py
@@ -22,15 +22,99 @@
 # The minute must be consist of two digits and may contain a leading zero, for example "10:2" is not valid,
 #   it should be "10:02".
 
+import itertools
+
+
 class Solution(object):
+    def get_group(self, r_num):
+        """
+        :param r_num:
+        :return:
+        """
+        lst = list()
+        for i in range(0, 5):
+            for j in range(0, 7):
+                if i + j == r_num:
+                    lst.append((i, j))
+
+        return lst
+
+    def get_binary_time(self, r_hour, r_min):
+        """
+        :param r_item:
+        :return:
+        """
+        rst_lst = list()
+        binary_hour_lst = self.get_binary_str(r_hour, 4)
+        # print binary_hour_lst
+        binary_min_lst = self.get_binary_str(r_min, 6)
+        # print binary_min_lst
+
+        for h in binary_hour_lst:
+            for m in binary_min_lst:
+                rst_lst.append((h, m))
+        # print rst_lst
+        return rst_lst
+
+    def get_binary_str(self, r_hour, r_max_num):
+        """
+
+        :param r_hour:
+        :return:
+        """
+        rst_lst = list()
+        h_lst = list()
+        b_h_lst = list()
+        for i in range(r_hour):
+            h_lst.append(1)
+        for i in range(r_max_num - r_hour):
+            h_lst.append(0)
+        gen_rst = itertools.permutations(h_lst)
+        for i in gen_rst:
+            # print ''.join([str(j) for j in i])
+            b_h_lst.append(''.join([str(j) for j in i]))
+        # print h_lst
+        b_h_lst = list(set(b_h_lst))
+
+        return b_h_lst
+
+    def get_real_time(self, r_item):
+        """
+        :param r_item:
+        :return:
+        """
+        hour = int(r_item[0], 2)
+        min = int(r_item[1], 2)
+        if hour > 11 or min > 59:
+            # print "#########################%d:%02d" % (hour, min)
+            tmp_time = None
+        else:
+            tmp_time = "%d:%02d" % (hour, min)
+
+        return tmp_time
+
     def readBinaryWatch(self, num):
         """
         :type num: int
         :rtype: List[str]
         """
         rst_lst = list()
+        possible_group = self.get_group(num)
+        # print possible_group
+        possible_binary_group = list()
+        for item in possible_group:
+            tmp = self.get_binary_time(item[0], item[1])
+            possible_binary_group += tmp
+
+        for i in possible_binary_group:
+            real_time = self.get_real_time(i)
+            if real_time is not None:
+                rst_lst.append(real_time)
+
+        print rst_lst
 
         return rst_lst
+
 
 if __name__ == '__main__':
     s = Solution()
@@ -38,3 +122,7 @@ if __name__ == '__main__':
     s.readBinaryWatch(2)
     s.readBinaryWatch(3)
     s.readBinaryWatch(4)
+    s.readBinaryWatch(5)
+    s.readBinaryWatch(6)
+    s.readBinaryWatch(7)
+    s.readBinaryWatch(8)
