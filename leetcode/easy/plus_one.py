8822c6d35917b4e2f35bc91b2450d2fc6339f737
gucb <chuan.gu6@gmail.com>
2016-09-27 18:08:43 +0800

    modified:   easy/plus_one.py 	new file:   easy/symmetric_tree.py

--- leetcode/easy/plus_one.py
+++ leetcode/easy/plus_one.py
@@ -12,8 +12,24 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
+        tmp = []
+        digits.reverse()
+        # print digits
+        up = 1
+        for i in digits:
+            if i + up > 9:
+                tmp.append(0)
+                up = (i + up) / 10
+            else:
+                tmp.append(i + up)
+                up = 0
+        tmp.reverse()
+        if tmp[0] == 0:
+            tmp.insert(0, 1)
+        return tmp
 
 
 if __name__ == '__main__':
     s = Solution()
-    s.plusOne([1, 2, 3, 4, 5, 6])
+    print s.plusOne([1, 2, 3, 4, 5, 6])
+    print s.plusOne([9, 9, 9, 9, 9, 9])
