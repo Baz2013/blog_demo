215554d5082372778284e2a02a2b9d4edc9cd1dd
gucb <chuan.gu6@gmail.com>
2016-09-27 16:43:11 +0800

    new file:   easy/linked_list_cycle.py

---
+++ leetcode/easy/linked_list_cycle.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+
+# 141. Linked List Cycle  QuestionEditorial Solution  My Submissions
+# Difficulty: Easy
+# Given a linked list, determine if it has a cycle in it.
+#
+# Follow up:
+# Can you solve it without using extra space?
+#
+# Subscribe to see which companies asked this question
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+from common.link_list import *
+
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+
+
+if __name__ == '__main__':
+    root = creat_link([])
+    s = Solution()
+    s.hasCycle(root)
