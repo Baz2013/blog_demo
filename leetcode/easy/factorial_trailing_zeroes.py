2bd8539684c1542fffaa9b3a419d7dcb92bd1114
gucb <chuan.gu6@gmail.com>
2016-10-12 19:48:52 +0800

    new file:   easy/factorial_trailing_zeroes.py

---
+++ leetcode/easy/factorial_trailing_zeroes.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+# 172. Factorial Trailing Zeroes  QuestionEditorial Solution  My Submissions
+# Difficulty: Easy
+# Given an integer n, return the number of trailing zeroes in n!.
+#
+# Note: Your solution should be in logarithmic time complexity.
+
+
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.trailingZeroes(1)
