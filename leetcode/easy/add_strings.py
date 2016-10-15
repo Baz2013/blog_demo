609bb2a33e74dff832bcdbe52f994ed4f58ad90e
gucb <chuan.gu6@gmail.com>
2016-10-15 11:45:42 +0800

    new file:   easy/add_strings.py 	modified:   easy/factorial_trailing_zeroes.py

---
+++ leetcode/easy/add_strings.py
@@ -0,0 +1,27 @@
+# -*- coding:utf-8 -*-
+#
+# 415. Add Strings   QuestionEditorial Solution  My Submissions
+# Difficulty: Easy
+# Contributors: Admin
+# Given two non-negative numbers num1 and num2 represented as string, return the sum of num1 and num2.
+#
+# Note:
+#
+# The length of both num1 and num2 is < 5100.
+# Both num1 and num2 contains only digits 0-9.
+# Both num1 and num2 does not contain any leading zero.
+# You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.addStrings('123', '456')
--- leetcode/easy/factorial_trailing_zeroes.py
+++ leetcode/easy/factorial_trailing_zeroes.py
@@ -4,7 +4,10 @@
 # Given an integer n, return the number of trailing zeroes in n!.
 #
 # Note: Your solution should be in logarithmic time complexity.
-
+# 10 = 2 * 5
+# example: 11! = 11 * 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+# 可以看出2的个数比5的个数多,所以只需要求出5的个数就是0的个数
+# from http://www.purplemath.com/modules/factzero.htm
 
 class Solution(object):
     def trailingZeroes(self, n):
@@ -12,8 +15,33 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        res = 0
+        while n:
+            res += n / 5
+            n /= 5
+
+        return res
+
+    def trailingZeroes_1(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res = 0
+        x = 5
+        while n >= x:
+            res += n / x
+            x *= 5
+
+        return res
 
 
 if __name__ == '__main__':
     s = Solution()
-    s.trailingZeroes(1)
+    print s.trailingZeroes(1)
+    print s.trailingZeroes(6)
+    print s.trailingZeroes(11)
+    print s.trailingZeroes(101)
+    print s.trailingZeroes(1001)
+    print s.trailingZeroes(4617)
+    print s.trailingZeroes_1(4617)
