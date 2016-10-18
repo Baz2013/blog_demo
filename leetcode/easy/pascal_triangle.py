5414efd0f6b664b55677f5e11ccec3ec8f82530d
gucb <chuan.gu6@gmail.com>
2016-10-18 16:00:32 +0800

    new file:   easy/pascal_triangle.py

---
+++ leetcode/easy/pascal_triangle.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+
+# 118. Pascal's Triangle   QuestionEditorial Solution  My Submissions
+# Difficulty: Easy
+# Contributors: Admin
+# Given numRows, generate the first numRows of Pascal's triangle.
+#
+# For example, given numRows = 5,
+# Return
+#
+# [
+#      [1],
+#     [1,1],
+#    [1,2,1],
+#   [1,3,3,1],
+#  [1,4,6,4,1]
+# ]
+
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.generate(5)
