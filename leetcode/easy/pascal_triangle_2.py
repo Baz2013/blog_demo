aa074de273dd32917521077ec1ef76f3df176573
gucb <chuan.gu6@gmail.com>
2016-10-19 09:58:38 +0800

    new file:   easy/pascal_triangle_2.py

---
+++ leetcode/easy/pascal_triangle_2.py
@@ -0,0 +1,26 @@
+# -*- coding:utf-8 -*-
+
+# 119. Pascal's Triangle II   QuestionEditorial Solution  My Submissions
+# Difficulty: Easy
+# Contributors: Admin
+# Given an index k, return the kth row of the Pascal's triangle.
+#
+# For example, given k = 3,
+# Return [1,3,3,1].
+#
+# Note:
+# Could you optimize your algorithm to use only O(k) extra space?
+#
+# Subscribe to see which companies asked this question
+
+class Solution(object):
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print s.getRow(3)
