9da588addfb0466b3414881bf833ebc3e75230ed
gucb <chuan.gu6@gmail.com>
2016-10-22 17:29:30 +0800

    modified:   easy/fiz_buzz.py

--- leetcode/easy/fiz_buzz.py
+++ leetcode/easy/fiz_buzz.py
@@ -5,7 +5,8 @@
 # Contributors: Admin
 # Write a program that outputs the string representation of numbers from 1 to n.
 #
-# But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+# But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”.
+# For numbers which are multiples of both three and five output “FizzBuzz”.
 #
 # Example:
 #
@@ -36,7 +37,18 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
+        res = list()
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                res.append('FizzBuzz')
+            elif i % 3 == 0:
+                res.append('Fizz')
+            elif i % 5 == 0:
+                res.append('Buzz')
+            else:
+                res.append(str(i))
 
+        return res
 
 if __name__ == '__main__':
     s = Solution()
